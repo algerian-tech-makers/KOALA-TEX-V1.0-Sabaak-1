ecd49fa1e08f5b4af8b01a51a9fa573939350dc1
tahar belghitri <103012653+TaharBelghitriDz@users.noreply.github.com>
2022-08-02 15:09:49 +0100

    initial

---
+++ Projects/backend/index.ts
@@ -0,0 +1,7 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import app from "./src";
+
+const PORT = process.env.PORT || 5005;
+app.listen(PORT, () => console.log("SERVER IS RUNNING ON PORT " + PORT));
