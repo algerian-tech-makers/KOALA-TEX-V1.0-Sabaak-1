0ed738fb3b23abc99cabf7f651ccba54e4ebc360
tahar belghitri <103012653+TaharBelghitriDz@users.noreply.github.com>
2022-08-06 01:14:00 +0100

    edit

---
+++ Projects/frontend/pages/_app.js
@@ -0,0 +1,8 @@
+import '../styles/globals.css'
+import {ChakraProvider} from "@chakra-ui/react"
+
+function MyApp({ Component, pageProps }) {
+  return <ChakraProvider><Component {...pageProps} /></ChakraProvider>
+}
+
+export default MyApp
