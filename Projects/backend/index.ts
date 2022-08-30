64bb0bd3d1db4a61e4077cb8ab993fe3e38cc559
tahar belghitri <tahartlm0@gmail.com>
2022-08-30 22:00:23 +0100

    edit backend

--- Projects/backend/index.ts
+++
@@ -1,9 +0,0 @@
-import dotenv from "dotenv";
-dotenv.config();
-
-import "./config/mongo.config";
-
-import app from "./src";
-
-const PORT = process.env.PORT || 5005;
-app.listen(PORT, () => console.log("SERVER IS RUNNING ON PORT " + PORT));
--- Projects/backend/src/helpers/res.helper.ts
+++
@@ -1,7 +0,0 @@
-import { Response } from "express";
-
-export const resHelper =
-  (res: Response) =>
-  (status = 400, json = { err: "something wrong happend" }) => {
-    res.status(status).json(json);
-  };
